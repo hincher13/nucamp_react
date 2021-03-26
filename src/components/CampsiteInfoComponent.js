bb7f8026226e56117d62ebcfc5277ddc15246c22
Ray Hinch <rahinch@icloud.com>
2021-03-26 15:55:33 -0400

    3. Exercise: Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -14,7 +14,7 @@ import { LocalForm, Control, Errors} from 'react-redux-form';
                  </div>)    
     }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -28,7 +28,7 @@ function RenderComments({comments}) {
                         </div>
                     );
                 })}
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
     }
@@ -52,7 +52,11 @@ function CampsiteInfo(props) {
             </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite}/>
-                    <RenderComments comments={props.comments}/>
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
@@ -80,7 +84,7 @@ class CommentForm extends Component {
         };
         
         this.toggleModal = this.toggleModal.bind(this);
-        this.handSubmit = this.handleSubmit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
 
     }
     
@@ -91,8 +95,10 @@ class CommentForm extends Component {
     }
     
     handleSubmit(values) {
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
+        this.toggleModal();
+        //When form is submitted, the add comment action creator will create an action using the values from this form.  Then that action will ge tdipsatched to the reducer which will update the state
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text)
+
     }
 
 
