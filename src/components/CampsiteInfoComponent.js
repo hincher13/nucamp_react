a5f2bb0bf0686dcb27c37082ce7d1c7b5c550e69
Ray Hinch <rahinch@icloud.com>
2021-06-09 17:22:28 -0400

    Exercise: React Animation Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,15 +3,22 @@ import {Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, M
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors} from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
     function RenderCampsite({campsite}) {
         return (<div className="col-md-5 m-1">
-                    <Card>
-                        <CardImg top src={campsite.image} alt={campsite.name} />
-                        <CardBody>
-                            <CardText>{campsite.description}</CardText>
-                        </CardBody>
-                    </Card>
+                    <FadeTransform
+                    in 
+                    transformProps={{
+                        exitTransform: 'scale(0.5) translateY(-50%)'
+                    }}>
+                        <Card>
+                            <CardImg top src={campsite.image} alt={campsite.name} />
+                            <CardBody>
+                                <CardText>{campsite.description}</CardText>
+                            </CardBody>
+                        </Card>
+                   </FadeTransform> 
                  </div>)    
     }
 
@@ -20,15 +27,19 @@ function RenderComments({comments, addComment, campsiteId}) {
         return (
             <div className="col-md-5 m-1">
                 <h4>Comments</h4>
-                {comments.map(comment=> {
-                    return(
-                        <div key={comment.id}>
-                            <p>{comment.text}<br />
-                                {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                            </p>
-                        </div>
-                    );
-                })}
+                <Stagger in>
+                    {comments.map(comment=> {
+                        return(
+                            <Fade in key={comment.id}>
+                                <div >
+                                    <p>{comment.text}<br />
+                                        {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                    </p>
+                                </div>
+                           </Fade> 
+                        );
+                    })}
+                </Stagger>
                 <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
+import { FadeTransform } from 'react-animation-components'
 
 function RenderCard({item, isLoading, errMess}) {
     if (isLoading) {
@@ -11,13 +12,19 @@ function RenderCard({item, isLoading, errMess}) {
     }
     
     return (
-        <Card>
-            <CardImg src={item.image} alt={item.name} />
-            <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
+        <FadeTransform
+        in
+        transformProps={{
+            exitTransform: 'scale(0.5) translateY(50%)'
+        }}>
+            <Card>
+                <CardImg src={item.image} alt={item.name} />
+                <CardBody>
+                    <CardTitle>{item.name}</CardTitle>
+                    <CardText>{item.description}</CardText>
+                </CardBody>
+            </Card>
+        </FadeTransform>     
     );
 }
 
