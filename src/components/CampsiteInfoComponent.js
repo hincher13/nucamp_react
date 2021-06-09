4108e25ef02336c00be4ca7cbc771f7e95ad1af1
Ray Hinch <rahinch@icloud.com>
2021-06-09 17:01:27 -0400

    Exercise: React Transitions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors} from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
     function RenderCampsite({campsite}) {
         return (<div className="col-md-5 m-1">
@@ -37,6 +38,27 @@ function RenderComments({comments, addComment, campsiteId}) {
 
     
 function CampsiteInfo(props) {
+    if(props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        )
+    }
+    if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
     if (props.campsite){
         return (
             <div className="container">
@@ -168,6 +190,5 @@ class CommentForm extends Component {
         )
     }
 }
-//test
 
 export default CampsiteInfo;
--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Control, LocalForm, Errors} from 'react-redux-form';
+import { Control, Form, Errors, actions} from 'react-redux-form';
 
 
 const required = val => val && val.length;
@@ -37,6 +37,13 @@ class Contact extends Component {
             handleSubmit(values) {
                 console.log("Current state is: " + JSON.stringify(values));
                 alert("Current state is: " + JSON.stringify(values));
+                this.props.resetFeedbackForm();
+            }
+
+            toggleModal() {
+                this.setState({
+                  isModalOpen: !this.state.isModalOpen
+                });
             }
 
             render() {
@@ -75,7 +82,7 @@ class Contact extends Component {
                         <hr />
                     </div>
                     <div className="col-md-10">
-                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                        <Form model="feedbackForm" onSubmit={values => this.handleSubmit(values)}>
                             <Row className="form-group">
                                 <Label htmlFor="firstName" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -202,6 +209,7 @@ class Contact extends Component {
                                 <Col md={10}>
                                     <Control.textarea model=".feedback" id="feedback" name="feedback"
                                         rows="12"
+                                        className="form-control"
                                     />
                                 </Col>
                             </Row>
@@ -212,7 +220,7 @@ class Contact extends Component {
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                     </div>
                 </div>
             </div>
